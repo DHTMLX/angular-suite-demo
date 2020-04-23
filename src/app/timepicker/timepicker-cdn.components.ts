e6828b396b2848f2ea45810eee2f50beecf6c827
Pavel Mihutski <pmihutski@xbsoftware.com>
2020-04-23 21:55:00 +0300

    added timpicker sample(cdn )

---
+++ src/app/timepicker/timepicker-cdn.components.ts
@@ -0,0 +1,35 @@
+import { Output, Component, ViewChild, OnDestroy, ElementRef, EventEmitter } from '@angular/core';
+import { Timepicker as TimepickerDHX } from 'dhx-suite';
+import fromCDN from 'from-cdn';
+
+@Component({
+    selector: 'app-timepicker-cdn',
+    template: `<div class="container"><div #widget class='widget-box-wide'></div></div>`,
+    styleUrls: [ './timepicker.scss' ],
+})
+export class TimePickerCDNComponent implements OnDestroy {
+    @ViewChild('widget', { static: true })
+    container: ElementRef;
+    timepicker: TimepickerDHX;
+    wait: Promise<void>;
+
+    @Output() ready: EventEmitter<any> = new EventEmitter();
+
+    constructor() {
+        this.wait = fromCDN([
+            'https://cdn.dhtmlx.com/suite/edge/suite.js',
+            'https://cdn.dhtmlx.com/suite/edge/suite.css',
+        ]).then(() => {
+            this.timepicker = new TimepickerDHX(this.container.nativeElement, {
+                css: 'dhx_widget--bordered',
+            });
+            this.ready.emit({ timepicker: this.timepicker });
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.timepicker) {
+            this.timepicker.destructor();
+        }
+    }
+}
