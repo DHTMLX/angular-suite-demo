0acaf7f3e688195cf8f6c4884a66e038ff4fb156
Pavel Mihutski <pmihutski@xbsoftware.com>
2020-04-24 16:57:06 +0300

    added events toolbar sample

--- src/stories/5-toolbar.stories.ts
+++ src/stories/5-toolbar.stories.ts
@@ -2,6 +2,7 @@ import { ToolbarComponent } from '../app/toolbar/toolbar.component';
 import { ToolbarConfiguratedComponent } from '../app/toolbar/toolbar-configurated.component';
 import { ToolbarDataComponent } from '../app/toolbar/toolbar-data.component';
 import { ToolbarCDNComponent } from '../app/toolbar/toolbar-cdn.component';
+import { ToolbarEventsComponent } from '../app/toolbar/toolbar-events.component';
 
 export default {
     title: 'Toolbar',
@@ -24,6 +25,6 @@ export const CDN = () => ({
     component: ToolbarCDNComponent,
 });
 
-// export const Events = () => ({
-//     component: SliderEventsComponent,
-// });
+export const Events = () => ({
+    component: ToolbarEventsComponent,
+});
