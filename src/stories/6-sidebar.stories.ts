f955b5c3c576e32c9866a691276d1bc20a347cd7
Pavel Mihutski <pmihutski@xbsoftware.com>
2020-04-30 13:23:50 +0300

    added cdn sample for sidebar

--- src/stories/6-sidebar.stories.ts
+++ src/stories/6-sidebar.stories.ts
@@ -1,7 +1,7 @@
 import { SidebarComponent } from '../app/sidebar/sidebar.component';
 import { SidebarConfiguratedComponent } from '../app/sidebar/sidebar-configurated';
 import { SidebarDataComponent } from '../app/sidebar/sidebar-data.component';
-// import { ToolbarCDNComponent } from '../app/toolbar/toolbar-cdn.component';
+import { SidebarCDNComponent } from '../app/sidebar/sidebar-cdn.component';
 // import { ToolbarEventsComponent } from '../app/toolbar/toolbar-events.component';
 
 export default {
@@ -21,9 +21,9 @@ export const Data = () => ({
     component: SidebarDataComponent,
 });
 
-// export const CDN = () => ({
-//     component: ToolbarCDNComponent,
-// });
+export const CDN = () => ({
+    component: SidebarCDNComponent,
+});
 
 // export const Events = () => ({
 //     component: ToolbarEventsComponent,
