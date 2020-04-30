baefc25f3db0510089ec4d368dc45b77ea6fadd6
Pavel Mihutski <pmihutski@xbsoftware.com>
2020-04-30 11:42:45 +0300

    added sidebar sample

---
+++ src/stories/6-sidebar.stories.ts
@@ -0,0 +1,30 @@
+import { SidebarComponent } from '../app/sidebar/sidebar.component';
+// import { ToolbarConfiguratedComponent } from '../app/toolbar/toolbar-configurated.component';
+// import { ToolbarDataComponent } from '../app/toolbar/toolbar-data.component';
+// import { ToolbarCDNComponent } from '../app/toolbar/toolbar-cdn.component';
+// import { ToolbarEventsComponent } from '../app/toolbar/toolbar-events.component';
+
+export default {
+    title: 'Sidebar',
+    component: SidebarComponent,
+};
+
+export const Base = () => ({
+    component: SidebarComponent,
+});
+
+// export const Configurated = () => ({
+//     component: ToolbarConfiguratedComponent,
+// });
+
+// export const Data = () => ({
+//     component: ToolbarDataComponent,
+// });
+
+// export const CDN = () => ({
+//     component: ToolbarCDNComponent,
+// });
+
+// export const Events = () => ({
+//     component: ToolbarEventsComponent,
+// });
