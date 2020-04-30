a51ba26ff470e5620cf1460f754378fb6cffbb83
Pavel Mihutski <pmihutski@xbsoftware.com>
2020-04-30 14:57:45 +0300

    added ribbon sample

---
+++ src/stories/7-ribbon.stories.ts
@@ -0,0 +1,30 @@
+import { RibbonComponent } from '../app/ribbon/ribbon.component';
+// import { SidebarConfiguratedComponent } from '../app/sidebar/sidebar-configurated';
+// import { SidebarDataComponent } from '../app/sidebar/sidebar-data.component';
+// import { SidebarCDNComponent } from '../app/sidebar/sidebar-cdn.component';
+// import { SidebarEventsComponent } from '../app/sidebar/sidebar-events.component';
+
+export default {
+    title: 'Ribbon',
+    component: RibbonComponent,
+};
+
+export const Base = () => ({
+    component: RibbonComponent,
+});
+
+// export const Configurated = () => ({
+//     component: SidebarConfiguratedComponent,
+// });
+
+// export const Data = () => ({
+//     component: SidebarDataComponent,
+// });
+
+// export const CDN = () => ({
+//     component: SidebarCDNComponent,
+// });
+
+// export const Events = () => ({
+//     component: SidebarEventsComponent,
+// });
