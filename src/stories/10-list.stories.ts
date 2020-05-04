4bbdeaf979c62e8a36711eda6d9fb266e86d1473
Pavel Mihutski <pmihutski@xbsoftware.com>
2020-05-04 18:09:15 +0300

    added base sample for grid component

---
+++ src/stories/10-list.stories.ts
@@ -0,0 +1,28 @@
+import { ListComponent } from '../app/list/list.component';
+
+
+
+export default {
+    title: 'List',
+    component: ListComponent,
+};
+
+export const Base = () => ({
+    component: ListComponent,
+});
+
+// export const Configurated = () => ({
+//     component: DataviewConfiguratedComponent,
+// });
+
+// // export const Data = () => ({
+// //     component: RibbonDataComponent,
+// // });
+
+// export const CDN = () => ({
+//     component: DataviewCDNComponent,
+// });
+
+// export const Events = () => ({
+//     component: DataviewEventsComponent,
+// });
