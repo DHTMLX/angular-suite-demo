e14cde49a63d7a29ed8f5b7510d247b398d2a75c
Pavel Mihutski <pmihutski@xbsoftware.com>
2020-05-04 13:56:13 +0300

    added dataview sample

---
+++ src/app/dataview/dataview.component.ts
@@ -0,0 +1,39 @@
+import { Output, Component, ViewChild, OnDestroy, ElementRef, EventEmitter } from '@angular/core';
+import { DataView as DataviewDHX, DataCollection } from 'dhx-suite';
+@Component({
+    selector: 'app-ribbon-common-cdn',
+    template: `<div class="container"><div #widget class='widget-box-wide'></div></div>`,
+    styleUrls: [ './dataview.scss' ],
+})
+export class DataViewComponent implements OnDestroy {
+    @ViewChild('widget', { static: true })
+    container: ElementRef;
+    toolbar: DataviewDHX;
+    wait: Promise<void>;
+
+    renderTemplate = (item) => `<div class='item_wrap item-wrap--grid'>
+    <img
+        class='image'
+        style="max-width: 150px"
+        src="https://dhtmlx.github.io/react-widgets/static/${item.img}"
+    />
+    <h2 class='title'>${item.title}</h2>
+    <div>${item.short}</div>
+  </div>`;
+
+    ngOnInit() {
+        this.toolbar = new DataviewDHX(this.container.nativeElement, {
+            css: 'dhx_widget--bordered dhx_widget--bg_white',
+            template: this.renderTemplate,
+            itemsInRow: 6
+        });
+
+        this.toolbar.data.load('https://dhtmlx.github.io/react-widgets/static/dataview.json');
+    }
+
+    ngOnDestroy() {
+        if (this.toolbar) {
+            this.toolbar.destructor();
+        }
+    }
+}
---
+++ src/stories/8-dataview.stories.ts
@@ -0,0 +1,27 @@
+import { DataViewComponent } from '../app/dataview/dataview.component';
+
+
+export default {
+    title: 'DataView',
+    component: DataViewComponent,
+};
+
+export const Base = () => ({
+    component: DataViewComponent,
+});
+
+// export const Configurated = () => ({
+//     component: RibbonConfiguratedComponent,
+// });
+
+// export const Data = () => ({
+//     component: RibbonDataComponent,
+// });
+
+// export const CDN = () => ({
+//     component: RibbonCDNComponent,
+// });
+
+// export const Events = () => ({
+//     component: RibbonEventsComponent,
+// });
