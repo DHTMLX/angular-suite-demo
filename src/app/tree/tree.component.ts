7618ce66a004a5c9d941358021cba74db11052c1
Pavel Mihutski <pmihutski@xbsoftware.com>
2020-05-05 17:08:02 +0300

    added base simple for tree component

---
+++ src/app/tree/tree.component.ts
@@ -0,0 +1,27 @@
+import { Output, Component, ViewChild, OnDestroy, ElementRef, EventEmitter } from '@angular/core';
+import { Tree as TreeDHX, TreeCollection } from 'dhx-suite';
+@Component({
+    selector: 'app-tree-cdn',
+    template: `<div class="container"><div #widget class='widget-box-wide'></div></div>`,
+    styleUrls: [ './tree.scss' ],
+})
+export class TreeComponent implements OnDestroy {
+    @ViewChild('widget', { static: true })
+    container: ElementRef;
+    tree: TreeDHX;
+    wait: Promise<void>;
+
+    ngOnInit() {
+        this.tree = new TreeDHX(this.container.nativeElement, {
+            css: 'dhx_widget--bordered dhx_widget--bg_white',
+        });
+
+        this.tree.data.load('https://dhtmlx.github.io/react-widgets/static/tree.json');
+    }
+
+    ngOnDestroy() {
+        if (this.tree) {
+            this.tree.destructor();
+        }
+    }
+}
---
+++ src/stories/15-tree.stories.ts
@@ -0,0 +1,10 @@
+import { TreeComponent } from '../app/tree/tree.component';
+
+export default {
+    title: 'Tree',
+    component: TreeComponent,
+};
+
+export const Base = () => ({
+    component: TreeComponent,
+});
