f0025463e27c1a6bb5bc2d7d096b8734ce815bf9
Pavel Mihutski <pmihutski@xbsoftware.com>
2020-05-05 14:23:47 +0300

    added base sample for form component

---
+++ src/stories/12-form.stories.ts
@@ -0,0 +1,28 @@
+import { FormComponent } from '../app/form/form.component';
+
+
+
+export default {
+    title: 'Form',
+    component: FormComponent,
+};
+
+export const Base = () => ({
+    component: FormComponent,
+});
+
+// export const Configurated = () => ({
+//     component: DataviewConfiguratedComponent,
+// });
+
+// // export const Data = () => ({
+// //     component: RibbonDataComponent,
+// // });
+
+// export const CDN = () => ({
+//     component: DataviewCDNComponent,
+// });
+
+// export const Events = () => ({
+//     component: DataviewEventsComponent,
+// });
