868ac33c7a5acdbb6b3a5c32af60352843859ab5
Pavel Mihutski <pmihutski@xbsoftware.com>
2020-05-05 18:08:56 +0300

    added base sample for message component

---
+++ src/stories/18-message.stories.ts
@@ -0,0 +1,10 @@
+import { MessageComponent } from '../app/message/message.component';
+
+export default {
+    title: 'Message',
+    component: MessageComponent,
+};
+
+export const Base = () => ({
+    component: MessageComponent,
+});
