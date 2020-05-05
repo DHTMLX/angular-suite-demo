1cef0075828a1b17ff811aac6873522eac16e97e
Pavel Mihutski <pmihutski@xbsoftware.com>
2020-05-05 18:27:02 +0300

    added base sample for popup component

---
+++ src/app/popup/popup.component.ts
@@ -0,0 +1,36 @@
+import { Output, Component, ViewChild, OnDestroy, ElementRef, EventEmitter } from '@angular/core';
+import { Popup as PopupDHX } from 'dhx-suite';
+
+@Component({
+    selector: 'app-window-cdn',
+    template: `<div class="container">
+        <div #widget class='widget-box-wide'>
+            
+        </div>
+        <button  (click)="this.popup.show(this.container)">
+            Show Popup
+            </button>
+    </div>`,
+    styleUrls: [ './popup.scss' ],
+})
+export class PopupComponent implements OnDestroy {
+    @ViewChild('widget', { static: true })
+    container: ElementRef;
+    popup: PopupDHX;
+    wait: Promise<void>;
+
+    ngOnInit() {
+        this.popup = new PopupDHX({
+            css: 'dhx_widget--bordered',
+        });
+        this.popup.attachHTML(
+            "<div style='padding: 16px; text-align: center'>Hi there, <br/> welcome to DHTMLX-react popup sample</div>",
+        );
+    }
+
+    ngOnDestroy() {
+        if (this.popup) {
+            this.popup.destructor();
+        }
+    }
+}
---
+++ src/stories/19-popup.stories.ts
@@ -0,0 +1,10 @@
+import { PopupComponent } from '../app/popup/popup.component';
+
+export default {
+    title: 'Popup',
+    component: PopupComponent,
+};
+
+export const Base = () => ({
+    component: PopupComponent,
+});
