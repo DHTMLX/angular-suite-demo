694b295c3e99863724d6ef236d951be355fa7e5a
Pavel Mihutski <pmihutski@xbsoftware.com>
2020-05-05 16:50:28 +0300

    added base sample for combobox component

---
+++ src/app/combobox/combobox.component.ts
@@ -0,0 +1,27 @@
+import { Output, Component, ViewChild, OnDestroy, ElementRef, EventEmitter } from '@angular/core';
+import { Combobox as ComboboxDHX, DataCollection } from 'dhx-suite';
+@Component({
+    selector: 'app-combobox-cdn',
+    template: `<div class="container"><div #widget class='widget-box-wide'></div></div>`,
+    styleUrls: [ './combobox.scss' ],
+})
+export class ComboboxComponent implements OnDestroy {
+    @ViewChild('widget', { static: true })
+    container: ElementRef;
+    combobox: ComboboxDHX;
+    wait: Promise<void>;
+
+    ngOnInit() {
+        this.combobox = new ComboboxDHX(this.container.nativeElement, {
+            placeholder: 'Click to choose',
+        });
+
+        this.combobox.data.load('https://dhtmlx.github.io/react-widgets/static/combobox.json');
+    }
+
+    ngOnDestroy() {
+        if (this.combobox) {
+            this.combobox.destructor();
+        }
+    }
+}
---
+++ src/stories/14-combobox.stories.ts
@@ -0,0 +1,28 @@
+import { ComboboxComponent } from '../app/combobox/combobox.component';
+
+
+
+export default {
+    title: 'Combobox',
+    component: ComboboxComponent,
+};
+
+export const Base = () => ({
+    component: ComboboxComponent,
+});
+
+// export const Configurated = () => ({
+//     component: DataviewConfiguratedComponent,
+// });
+
+// // export const Data = () => ({
+// //     component: RibbonDataComponent,
+// // });
+
+// export const CDN = () => ({
+//     component: DataviewCDNComponent,
+// });
+
+// export const Events = () => ({
+//     component: DataviewEventsComponent,
+// });
