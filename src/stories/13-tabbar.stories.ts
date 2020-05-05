df665a91562b0b4e91f9806035105af70ecf100b
Pavel Mihutski <pmihutski@xbsoftware.com>
2020-05-05 16:16:34 +0300

    added base sample for tabbar component

---
+++ src/stories/13-tabbar.stories.ts
@@ -0,0 +1,28 @@
+import { TabbarComponent } from '../app/tabbar/tabbar.component';
+
+
+
+export default {
+    title: 'Tabbar',
+    component: TabbarComponent,
+};
+
+export const Base = () => ({
+    component: TabbarComponent,
+});
+
+// export const Configurated = () => ({
+//     component: DataviewConfiguratedComponent,
+// });
+
+// // export const Data = () => ({
+// //     component: RibbonDataComponent,
+// // });
+
+// export const CDN = () => ({
+//     component: DataviewCDNComponent,
+// });
+
+// export const Events = () => ({
+//     component: DataviewEventsComponent,
+// });
