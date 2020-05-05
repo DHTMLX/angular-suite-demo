bf0e1a8dc4c0cecf3abccd42571661363f3561e3
Pavel Mihutski <pmihutski@xbsoftware.com>
2020-05-05 17:13:27 +0300

    added base sample for chart component

---
+++ src/app/chart/chart.component.ts
@@ -0,0 +1,47 @@
+import { Output, Component, ViewChild, OnDestroy, ElementRef, EventEmitter } from '@angular/core';
+import { Chart as ChartDHX, DataCollection } from 'dhx-suite';
+@Component({
+    selector: 'app-chart-cdn',
+    template: `<div class="container"><div #widget class='widget-box-wide'></div></div>`,
+    styleUrls: [ './chart.scss' ],
+})
+export class ChartComponent implements OnDestroy {
+    @ViewChild('widget', { static: true })
+    container: ElementRef;
+    chart: ChartDHX;
+    wait: Promise<void>;
+
+    ngOnInit() {
+        this.chart = new ChartDHX(this.container.nativeElement, {
+            type: 'bar',
+            scales: {
+                bottom: {
+                    text: 'month',
+                },
+                left: {
+                    maxTicks: 10,
+                    max: 100,
+                    min: 0,
+                },
+            },
+            series: [
+                {
+                    id: 'A',
+                    value: 'company C',
+                    color: '#5E83BA',
+                    pointType: 'circle',
+                    fill: '#5E83BA',
+                    barWidth: 35,
+                },
+            ],
+        });
+
+        this.chart.data.load('https://dhtmlx.github.io/react-widgets/static/chart.json');
+    }
+
+    ngOnDestroy() {
+        if (this.chart) {
+            this.chart.destructor();
+        }
+    }
+}
---
+++ src/stories/16-chart.stories.ts
@@ -0,0 +1,10 @@
+import { ChartComponent } from '../app/chart/chart.component';
+
+export default {
+    title: 'Chart',
+    component: ChartComponent,
+};
+
+export const Base = () => ({
+    component: ChartComponent,
+});
