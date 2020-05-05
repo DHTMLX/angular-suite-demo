699959534b4759770972493df47fbb1b6e4cdcae
Pavel Mihutski <pmihutski@xbsoftware.com>
2020-05-05 13:52:32 +0300

    added base  sample for menu component

---
+++ src/stories/11-menu.stories.ts
@@ -0,0 +1,28 @@
+import { MenuComponent } from '../app/menu/menu.component';
+
+
+
+export default {
+    title: 'Menu',
+    component: MenuComponent,
+};
+
+export const Base = () => ({
+    component: MenuComponent,
+});
+
+// export const Configurated = () => ({
+//     component: DataviewConfiguratedComponent,
+// });
+
+// // export const Data = () => ({
+// //     component: RibbonDataComponent,
+// // });
+
+// export const CDN = () => ({
+//     component: DataviewCDNComponent,
+// });
+
+// export const Events = () => ({
+//     component: DataviewEventsComponent,
+// });
