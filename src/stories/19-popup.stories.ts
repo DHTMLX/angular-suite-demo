c500ac6f867137585904264513e57d80e6ac1346
accountF <kittylora.33@gmail.com>
2020-05-12 18:43:27 +0300

    added event sample for popup component

--- src/stories/19-popup.stories.ts
+++ src/stories/19-popup.stories.ts
@@ -1,4 +1,5 @@
 import { PopupComponent } from '../app/popup/popup.component';
+import { PopupEventComponent } from '../app/popup/popup-event.component';
 
 export default {
     title: 'Popup',
@@ -7,4 +8,8 @@ export default {
 
 export const Base = () => ({
     component: PopupComponent,
+});
+
+export const Events = () => ({
+    component: PopupEventComponent,
 });
