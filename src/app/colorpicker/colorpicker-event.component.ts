a31f0bef6a1a8b8dadd53d4aa3e16f90ccb21523
accountF <kittylora.33@gmail.com>
2020-05-13 15:26:24 +0300

    Updated cdn & event samples for colorpicker component. Also added base & configured samples.

--- src/app/colorpicker/colorpicker-event.component.ts
+++ src/app/colorpicker/colorpicker-event.component.ts
@@ -1,21 +1,19 @@
 import {Output, Component, ViewChild, OnDestroy, ElementRef, EventEmitter} from '@angular/core';
-import fromCDN from 'from-cdn';
-
-declare const dhx;
+import {Colorpicker as ColorpickerDHX} from 'dhx-suite';
 
 @Component({
   selector: 'app-colorpicker-event-cdn',
   template: `
-    <div class="component-wrapper">
-      <div #widget class='widget-box-wide'></div>
-      <div class="events-list-wrapper">
-        <div class="events-list--element" *ngIf="this.eventsList.length == 0">No events yet</div>
-        <div class="events-list--element" *ngFor='let event of eventsList'>
-          <p>{{event.name}}</p>
-          <p>{{event.payload}}</p>
-        </div>
-      </div>
-    </div>`,
+      <div class="component-wrapper">
+          <div #widget class='widget-box-wide'></div>
+          <div class="events-list-wrapper">
+              <div class="events-list--element" *ngIf="this.eventsList.length == 0">No events yet</div>
+              <div class="events-list--element" *ngFor='let event of eventsList'>
+                  <p>{{event.name}}</p>
+                  <p>{{event.payload}}</p>
+              </div>
+          </div>
+      </div>`,
   styleUrls: ['../app.component.scss'],
 })
 export class ColorpickerEventComponent implements OnDestroy {
@@ -32,18 +30,13 @@ export class ColorpickerEventComponent implements OnDestroy {
     this.eventsList = [{name, payload}].concat(this.eventsList);
   };
 
-  constructor() {
-    this.wait = fromCDN([
-      'https://cdn.dhtmlx.com/suite/edge/suite.js',
-      'https://cdn.dhtmlx.com/suite/edge/suite.css',
-    ]).then(() => {
-      this.colorPicker = new dhx.Colorpicker(this.container.nativeElement);
-      this.ready.emit({colorPicker: this.colorPicker});
-      this.colorPicker.events.on('change', (color) => this.logEvent(color, 'change'));
-      this.colorPicker.events.on('modeChange', (mode) => this.logEvent(mode, 'modeChange'));
-      this.colorPicker.events.on('cancelClick', () => this.logEvent(null, 'cancelClick'));
-      this.colorPicker.events.on('apply', () => this.logEvent(null, 'apply'));
-    });
+  ngOnInit() {
+    this.colorPicker = new ColorpickerDHX(this.container.nativeElement);
+    this.ready.emit({colorPicker: this.colorPicker});
+    this.colorPicker.events.on('change', (color) => this.logEvent(color, 'change'));
+    this.colorPicker.events.on('modeChange', (mode) => this.logEvent(mode, 'modeChange'));
+    this.colorPicker.events.on('cancelClick', () => this.logEvent(null, 'cancelClick'));
+    this.colorPicker.events.on('apply', () => this.logEvent(null, 'apply'));
   }
 
   ngOnDestroy() {
