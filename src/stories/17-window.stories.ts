0302121ce63486b3355ca331f6cc9552386412e3
accountF <kittylora.33@gmail.com>
2020-05-14 10:25:17 +0300

    Added cdn sample for window component. Also updated base sample.

--- src/stories/17-window.stories.ts
+++ src/stories/17-window.stories.ts
@@ -1,5 +1,6 @@
 import { WindowComponent } from '../app/window/window.component';
-import { WindowEventComponent } from "../app/window/window-event.component";
+import { WindowEventComponent } from '../app/window/window-event.component';
+import { WindowCDNComponent } from '../app/window/window-cdn.component';
 
 export default {
     title: 'Window',
@@ -8,6 +9,10 @@ export default {
 
 export const Base = () => ({
     component: WindowComponent,
+});
+
+export const CDN = () => ({
+    component: WindowCDNComponent,
 });
 
 export const Events = () => ({
