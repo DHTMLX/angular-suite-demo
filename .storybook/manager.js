dd5b8ddac4cce297b30d13734c573c93b689c884
Oleg <sorokin.world@gmail.com>
2020-06-15 10:49:56 +0300

    [dev] Angular demo - update UI

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,16 @@
 import { addons } from '@storybook/addons';
+import { create } from '@storybook/theming/create';
+
+const theme = create({
+  base: 'light',
+
+  brandTitle: 'DHTMLX Widgets',
+  brandUrl: 'https://dhtmlx.github.io/angular-suite-demo/?path=/story',
+  brandImage: './assets/logo.svg',
+});
 
 addons.setConfig({
   showPanel: false,
   enableShortcuts: false,
+  theme
 });
